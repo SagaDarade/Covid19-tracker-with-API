6a4635f616467befd0238bd96bb8e8c0b72a0e7b
SagaDarade <daradesagar42@gmail.com>
2020-09-26 13:31:38 +0530

    intial commit

---
+++ app/src/main/java/com/example/covid19tracker/FlashScam.java
@@ -0,0 +1,40 @@
+package com.example.covid19tracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.support.v7.app.AppCompatActivity;
+
+//import androidx.appcompat.app.AppCompatActivity;
+
+public class FlashScam extends AppCompatActivity implements Runnable {
+
+    Thread t;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_scam);
+        t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+
+        try
+        {
+            Thread.sleep(3000);
+        }
+        catch (Exception ee)
+        {
+            ee.printStackTrace();
+        }
+        Intent i=new Intent(FlashScam.this, MainActivity.class);
+        startActivity(i);
+        finish();
+
+    }
+}
